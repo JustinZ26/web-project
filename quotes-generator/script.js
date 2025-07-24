93c238a3503b407f0128fe204492c26161cef07f
Justin Zhang <justin89zhang@gmail.com>
2025-07-24 22:40:35 +0700

    added a simple click animation

--- quotes-generator/script.js
+++ quotes-generator/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("click", function(){
     console.log("page is clicked");
     getQuote();
+    hitMark(mouse.x, mouse.y, 15);
 })
 
 const quoteContainer = document.getElementById("quote");
@@ -21,3 +22,74 @@ async function getQuote(){
         console.log(`error dumbass: ${e}`);
     }
 }
+
+//click animation
+
+/** @type {HTMLCanvasElement} */
+let canvas = document.querySelector("#click-animation");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let c = canvas.getContext("2d");
+
+let mouse = {
+    x: undefined,
+    y: undefined
+};
+
+document.addEventListener("mousemove", function(e){
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+})
+
+function hitMark(x, y, lineLength){
+    this.x = x;
+    this.y = y;
+    this.lineLength = lineLength;
+
+    this.draw = function(){
+        c.beginPath();
+        c.moveTo(mouse.x, mouse.y);
+        c.lineTo(mouse.x - lineLength, mouse.y - lineLength);
+        c.strokeStyle = "white";
+        c.stroke();
+
+        c.beginPath();
+        c.moveTo(mouse.x, mouse.y);
+        c.lineTo(mouse.x + lineLength, mouse.y - lineLength);
+        c.strokeStyle = "white";
+        c.stroke();
+
+        c.beginPath();
+        c.moveTo(mouse.x, mouse.y);
+        c.lineTo(mouse.x - lineLength, mouse.y + lineLength);
+        c.strokeStyle = "white";
+        c.stroke();
+
+        c.beginPath();
+        c.moveTo(mouse.x, mouse.y);
+        c.lineTo(mouse.x + lineLength, mouse.y + lineLength);
+        c.strokeStyle = "white";
+        c.stroke();
+
+        setTimeout(() => {
+            c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        }, 75);
+    }
+
+    this.draw();
+
+    
+}
+
+
+
+// function animate(){
+//     requestAnimationFrame(animate);
+
+//     c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    
+
+// }
