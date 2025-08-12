a583d98e9293f5c914117b43982a7eb6c9a0436c
Justin Zhang <justin89zhang@gmail.com>
2025-08-12 20:14:11 +0700

    added animation and win condition

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -25,7 +25,63 @@ switch (randomChoice) {
         break;
 }
 
+const imagesLocation = {
+    0: "images/paper.png",
+    1: "images/rock.png",
+    2: "images/scissor.png"
+}
+
+function slotEffect(choice){
+    return new Promise(resolve => {
+        let spins = 35;   // how many image changes
+        let baseDelay = 100; // ms between changes
+        let totalTime = 0;
+
+        for (let i = 0; i <= spins; i++) {
+            totalTime += baseDelay;
+            console.log(totalTime);
+
+            setTimeout(() => {
+                if (i === spins) {
+                    oppChoice.src = imagesLocation[choice]; // final stop
+                    resolve(); // signal animation complete
+                } else {
+                    oppChoice.src = imagesLocation[i % 3];
+                }
+            }, totalTime);
+        }
+    });
+}
+
+
+
 let choiceArray = document.querySelectorAll("button");
+
+function pickRandomChoice(){
+    randomChoice = Math.floor(Math.random() * 3);
+
+    switch (randomChoice) {
+        case 0:
+            oppChoice.src = "images/paper.png";
+            theirPick = "paper";
+            break;
+
+        case 1:
+            oppChoice.src = "images/rock.png";
+            theirPick = "rock";
+            break;
+
+        case 2:
+            oppChoice.src = "images/scissor.png";
+            theirPick = "scissor";
+            break;
+
+        default:
+
+            break;
+    }
+
+}
 
 function removeChoosenExceptThisOne(index){
     for(let i = 0; i < choiceArray.length;i++){
@@ -46,6 +102,7 @@ for(let i = 0; i < choiceArray.length; i ++){
         choiceArray[i].classList.add("choosen");
         removeChoosenExceptThisOne(i);
         document.getElementById("result-container").style.display = "none";
+        pickRandomChoice();
     })
 }
 
@@ -53,7 +110,8 @@ console.log(choiceArray);
 
 let startButton = document.getElementById("start-button");
 
-function calculateWinner(){
+async function calculateWinner(){
+    await slotEffect(randomChoice);
     let playerChoice = null;
     for(let i = 0; i < choiceArray.length; i++){
         if(choiceArray[i].classList.contains("choosen")){
@@ -94,7 +152,6 @@ function calculateWinner(){
     }
 
     removeChoosenFromAll();
-
 }
 
 
