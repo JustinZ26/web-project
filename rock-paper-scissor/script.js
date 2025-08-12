323616536c69670c675e242d005674950b6be854
Justin Zhang <justin89zhang@gmail.com>
2025-08-12 19:10:50 +0700

    added logic, need fixing

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -25,4 +25,80 @@ switch (randomChoice) {
         break;
 }
 
+let choiceArray = document.querySelectorAll("button");
+
+function removeChoosenExceptThisOne(index){
+    for(let i = 0; i < choiceArray.length;i++){
+        if(i != index){
+            choiceArray[i].classList.remove("choosen");
+        }
+    }
+}
+
+function removeChoosenFromAll(){
+    for(let i = 0; i < choiceArray.length;i++){
+        choiceArray[i].classList.remove("choosen");
+    }
+}
+
+for(let i = 0; i < choiceArray.length; i ++){
+    choiceArray[i].addEventListener("click", function(){
+        choiceArray[i].classList.add("choosen");
+        removeChoosenExceptThisOne(i);
+        document.getElementById("result-container").style.display = "none";
+    })
+}
+
+console.log(choiceArray);
+
+let startButton = document.getElementById("start-button");
+
+function calculateWinner(){
+    let playerChoice = null;
+    for(let i = 0; i < choiceArray.length; i++){
+        if(choiceArray[i].classList.contains("choosen")){
+            playerChoice = i;
+        }
+    }
+
+    if(playerChoice == null){ //if player did not choose anything
+        return;
+    }
+
+    if(playerChoice == randomChoice){
+        //draw
+        let container = document.getElementById("result-container");
+        container.style.display = "flex";
+        container.style.backgroundColor = "grey";
+
+        document.getElementById("result").innerText = "Draw";
+    }
+    else if(playerChoice == 0 && randomChoice == 1 ||
+        playerChoice == 1 && randomChoice == 2||
+        playerChoice == 2 && randomChoice == 0
+    ){
+        //player win
+        let container = document.getElementById("result-container");
+        container.style.display = "flex";
+        container.style.backgroundColor = "green";
+
+        document.getElementById("result").innerText = "You Win";
+    }
+    else{
+        //player lose
+        let container = document.getElementById("result-container");
+        container.style.display = "flex";
+        container.style.backgroundColor = "red";
+
+        document.getElementById("result").innerText = "You Lose";
+    }
+
+    removeChoosenFromAll();
+
+}
+
+
+startButton.addEventListener("click", calculateWinner);
+
+
 
